5ea7d4833a50770435ba023f60264e9598fa257f
Phuchu97 <103049585+Phuchu97@users.noreply.github.com>
2023-07-16 22:00:39 +0700

    Update ApiConstant.js

--- src/Constants/ApiConstant.js
+++ src/Constants/ApiConstant.js
@@ -1,2 +1,2 @@
 // export const API_URL = 'http://localhost:4001'
-export const API_URL = 'https://funix-chat-backend.onrender.com/'
+export const API_URL = 'https://funix-chat-backend.onrender.com'
